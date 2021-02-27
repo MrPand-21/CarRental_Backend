78c2a4ded841f36e308caed39c46900eb96fccae
MrPand <alperacar751@gmail.com>
2021-02-27 21:19:20 +0300

    .

--- Core/Utilities/Interceptors/MethodInterception.cs
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -1,23 +1,8 @@
 ﻿using System;
-using System.Linq;
-using System.Reflection;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using System.Text;
 using Castle.DynamicProxy;
 
 namespace Core.Utilities.Interceptors
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
-    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
-    {
-        public int Priority { get; set; }
-
-        public virtual void Intercept(IInvocation invocation)
-        {
-
-        }
-    }
-
     public abstract class MethodInterception : MethodInterceptionBaseAttribute
     {
         protected virtual void OnBefore(IInvocation invocation) { }
@@ -46,19 +31,6 @@ namespace Core.Utilities.Interceptors
                 }
             }
             OnAfter(invocation);
-        }
-    }
-    public class AspectInterceptorSelector : IInterceptorSelector
-    {
-        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
-        {
-            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
-                (true).ToList();
-            var methodAttributes = type.GetMethod(method.Name)
-                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
-            classAttributes.AddRange(methodAttributes);
-
-            return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
     }
 }
